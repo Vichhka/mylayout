74bde0f94552c7bb0461c91699f03c40642c7b1e
Vichhka <somvichhka6@gmail.com>
2025-02-19 22:10:43 +0700

    layout-1

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  test: 'echo "Error: no test specified" && exit 1',
+  content: ["./**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
