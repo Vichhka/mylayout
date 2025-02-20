f6346d31943b1abe8e093cfe0c75be8dc048c02b
Vichhka <100078229+Vichhka@users.noreply.github.com>
2025-02-20 19:21:23 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   test: 'echo "Error: no test specified" && exit 1',
-  content: ["./**/*.html"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
